847577a4551cbceee6fec3b6e1cc52a4cc135be8
Utsav Pandey <95593979+utsavosho@users.noreply.github.com>
2022-01-18 01:13:19 +0100

    Add files via upload

---
+++ sendanemailwithpython.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import smtplib
+
+sender = "sender@gmail.com"
+recipient = "codewithtomi@gmail.com"
+password = "sender_password"
+subject = "Test email from Python"
+text = "Hello from Python"
+
+smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+smtp_server.login(sender, password)
+message = "Subject: {}\n\n{}".format(subject, text)
+smtp_server.sendmail(sender, recipient, message)
+smtp_server.close()
+
